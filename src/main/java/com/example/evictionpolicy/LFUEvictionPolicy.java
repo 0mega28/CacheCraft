e5908d79d15a44c481a615c3b3b6c1b874433ca0
Saksham Gupta <sakshamdam54321@gmail.com>
2024-08-10 18:10:25 +0530

    LFUEvictionPolicy directly implements EvictionPolicy

--- src/main/java/com/example/evictionpolicy/LFUEvictionPolicy.java
+++ src/main/java/com/example/evictionpolicy/LFUEvictionPolicy.java
@@ -2,7 +2,17 @@ package com.example.evictionpolicy;
 
 import org.jetbrains.annotations.NotNull;
 
-public class LFUEvictionPolicy<K> extends OrderBasedEvictionPolicy<K> {
+public class LFUEvictionPolicy<K> implements EvictionPolicy<K> {
+    @Override
+    public void keyAdded(@NotNull K key) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void keyRemoved(@NotNull K keyToEvict) {
+        throw new UnsupportedOperationException();
+    }
+
     @Override
     public void keyAccessed(@NotNull K key) {
         throw new UnsupportedOperationException();
